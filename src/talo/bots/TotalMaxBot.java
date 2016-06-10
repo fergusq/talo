372a93acf1ca0c9dc64b20fc09e8e7ece70bb8a8
Iikka Hauhio <iikka@kaivos.org>
2016-06-10 15:34:51 +0300

    lisätty totalmaxbot

---
+++ src/talo/bots/TotalMaxBot.java
@@ -0,0 +1,31 @@
+package talo.bots;
+
+import talo.Player;
+import talo.PlayerDetails;
+import talo.History;
+import talo.util.Effect;
+
+import java.util.Random;
+
+/**
+ * Wishes to have a house that is a little better than the previous best house.
+ */
+@PlayerDetails(name="Paras", color=Effect.Color.BLUE)
+public class TotalMaxBot implements Player {
+	private double factor = 1.5;
+	public int getWish(History history) {
+		long sum = 0;
+		for (int i = Math.max(10, history.rounds()-10); i < history.rounds(); i++) {
+			sum += history.getWishesOnRound(i).stream()
+				.mapToInt(w -> w.roundPoints).max().getAsInt();
+		}
+		final long finalSum = sum;
+		if (history.rounds() % 10 == 0) {
+			factor = history.withLastRound(
+				r -> (double) r.stream().mapToInt(w -> w.roundPoints).max().orElse(1) / finalSum * 10,
+				() -> 1.5
+			);
+		}
+		return sum == 0 ? (new Random()).nextInt(1000) : (int) (finalSum / 10 * factor);
+	}
+}
