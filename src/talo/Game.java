e3d9e9b21175869bfef30ca24b944253ebd26d48
Iikka Hauhio <iikka@kaivos.org>
2016-06-10 19:48:29 +0300

    parempi tulostaulu

--- src/talo/Game.java
+++ src/talo/Game.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
+import static java.util.stream.Collectors.joining;
+import java.util.function.Function;
+
 import talo.util.Effect;
 
 public class Game {
 	
 	private ArrayList<Class<? extends Player>> playerClasses = new ArrayList<>();
 	private HashMap<Class<? extends Player>, String> comments = new HashMap<>();
+	private HashMap<Class<? extends Player>, Integer> wishes = new HashMap<>();
 	private History history;
 	
 	public void addPlayer(Class<? extends Player> player) {
@@ -38,6 +42,7 @@ public class Game {
 			ArrayList<WishPair> wishes = new ArrayList<>();
 			for (Player p : players) {
 				int wish = p.getWish(history);
+				this.wishes.put(p.getClass(), wish);
 				if (wish < 0) wish = 0;
 				wishes.add(new WishPair(p, wish));
 			}
@@ -60,32 +65,62 @@ public class Game {
 		System.out.println();
 	}
 	
-	public void printScores() {
-		System.out.println(Effect.get().bold()
-			+ String.format("%1$-25s │ %2$12s │ %3$10s │ %4$14s │ %5$13s │ %6$6s │ %7$15s",
-					"BOT",
-					"TOTAL POINTS", "AVG POINTS",
-					"TOTAL WISH", "AVG WISH",
-					"WIN %", "COMMENT")
-			+ Effect.reset());
-		
-		List<Map.Entry<Class<? extends Player>, Long>> entries = new ArrayList<>(history.getPointsMap().entrySet());
-		entries.sort((a, b) -> b.getValue().compareTo(a.getValue()));
-		
-		for (Map.Entry<Class<? extends Player>, Long> score : entries) {
-			PlayerDetails details = score.getKey().getAnnotation(PlayerDetails.class);
-			Effect.Color bgcolor = details == null ? Effect.Color.BLACK : details.color();
-			String name = details == null ? score.getKey().getSimpleName() : details.name();
-			System.out.println(Effect.get().bgcolor(bgcolor).color(Effect.Color.WHITE)
-				+ String.format("%1$-25s │ %2$12d │ %3$10.2f │ %4$14d │ %5$13.2f │ %6$6.2f │ %7$15s",
-						name,
-						score.getValue(),
-						history.getAveragePoints(score.getKey()),
-						history.getTotalWish(score.getKey()),
-						history.getAverageWish(score.getKey()),
-						history.getWinningPercent(score.getKey()),
-						comments.get(score.getKey()))
-				+ Effect.reset());
+	class Scoreboard {
+		private class Column {
+			private String name, nameFormat, dataFormat;
+			private Function<Class<? extends Player>, Object> data;
 		}
+		private ArrayList<Column> columns = new ArrayList<>();
+		
+		public void addColumn(String nameFormat, String dataFormat,
+				String name, Function<Class<? extends Player>, Object> data) {
+			Column c = new Column();
+			c.name = name;
+			c.nameFormat = nameFormat;
+			c.dataFormat = dataFormat;
+			c.data = data;
+			columns.add(c);			
+		}
+		
+		public void printScores() {
+			String nameFormat = columns.stream().map(c -> c.nameFormat).collect(joining(" │ "));
+			Object[] names = columns.stream().map(c -> c.name).toArray();
+			System.out.println(Effect.get().bold()
+				+ String.format(nameFormat, names)
+				+ Effect.reset());
+				
+			List<Map.Entry<Class<? extends Player>, Long>> entries
+				= new ArrayList<>(history.getPointsMap().entrySet());
+			entries.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+			
+			for (Map.Entry<Class<? extends Player>, Long> score : entries) {
+				PlayerDetails details = score.getKey().getAnnotation(PlayerDetails.class);
+				Effect.Color bgcolor = details == null ? Effect.Color.BLACK : details.color();
+				
+				String dataFormat = columns.stream().map(c -> c.dataFormat).collect(joining(" │ "));
+				Object[] datas = columns.stream().map(c -> c.data.apply(score.getKey())).toArray();
+				
+				System.out.println(Effect.get().bgcolor(bgcolor).color(Effect.Color.WHITE)
+					+ String.format(dataFormat, datas)
+					+ Effect.reset());
+			}
+		}
+	}
+	
+	public void printScores() {
+		Scoreboard board = new Scoreboard();
+		board.addColumn("%-25s", "%-25s", "BOT", c -> {
+			PlayerDetails details = c.getAnnotation(PlayerDetails.class);
+			return details == null ? c.getSimpleName() : details.name();
+		});
+		board.addColumn("%12s", "%12d", "TOTAL POINTS", history::getPoints);
+		board.addColumn("%10s", "%10.2f", "AVG POINTS", history::getAveragePoints);
+		board.addColumn("%14s", "%14d", "TOTAL WISH", history::getTotalWish);
+		board.addColumn("%13s", "%13.2f", "AVG WISH", history::getAverageWish);
+		board.addColumn("%10s", "%10d", "LAST WISH", wishes::get);
+		board.addColumn("%6s", "%6.2f", "WIN %", history::getWinningPercent);
+		board.addColumn("%15s", "%15s", "COMMENT", comments::get);
+		
+		board.printScores();
 	}
 }
--- src/talo/bots/TotalMaxBot.java
+++ src/talo/bots/TotalMaxBot.java
@@ -15,12 +15,12 @@ public class TotalMaxBot implements Player {
 	private double factor = 1.5;
 	public int getWish(History history) {
 		long sum = 0;
-		for (int i = Math.max(10, history.rounds()-10); i < history.rounds(); i++) {
+		for (int i = Math.max(0, history.rounds()-10); i < history.rounds(); i++) {
 			sum += history.getWishesOnRound(i).stream()
 				.mapToInt(w -> w.roundPoints).max().getAsInt();
 		}
 		final long finalSum = sum;
-		if (history.rounds() % 2 == 0) {
+		if (finalSum != 0 && history.rounds() % 2 == 0) {
 			factor = history.withLastRound(
 				r -> (double) r.stream().mapToInt(w -> w.roundPoints).max().orElse(1) / finalSum * 10,
 				() -> 1.5
