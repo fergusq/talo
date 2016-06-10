f1be691eb2c77f8349b4ecd5ea5977d81dcc2291
Iikka Hauhio <iikka@kaivos.org>
2016-06-10 23:48:55 +0300

    lisätty saatana

---
+++ src/talo/bots/Satan.java
@@ -0,0 +1,26 @@
+package talo.bots;
+
+import java.util.Map;
+
+import talo.History;
+import talo.Player;
+import talo.PlayerDetails;
+import talo.util.Effect;
+
+/** Prevails over all by rewriting history.*/
+@PlayerDetails(name="Satan", color=Effect.Color.BLUE)
+public class Satan implements Player {
+
+	@Override
+	public int getWish(History history) {
+		if (history.rounds() % 6 < 3)
+			return 666;
+		else
+			return history.withLastRound(
+				r -> r.stream().mapToInt(w -> w.roundPoints).max().orElse(666) - 2,
+				() -> 666
+			);
+	}
+	
+	
+}
